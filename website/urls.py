dd0e1949a7dbddd220ce4fdcf4fa88615404b9a2
Amirgolbini <amirgol127@gmail.com>
2024-08-03 12:13:57 +0330

    home work
    adding home
    about
    contact page for the website

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-    path('http-test/', http_test)
+    path('',home),
+    path('about/',about),
+    path('contact/',contact)
+
 ]
--- website/views.py
+++ website/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def http_test(request):
-    return HttpResponse("hello")
+def home(request):
+    return HttpResponse("Home Page")
+
+def about(request):
+    return HttpResponse('About us')
+
+def contact(request):
+    return HttpResponse('Contact Us')
